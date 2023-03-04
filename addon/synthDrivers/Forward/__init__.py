76629c299268f36ddeb478d3aa856fa2ccce6189
rmcpantoja <angelitomateocedillo@gmail.com>
2023-03-03 22:31:01 -0500

    Work: Attempting to fix rate and pitch options. Part 1.1.

--- addon/synthDrivers/Forward/__init__.py
+++ addon/synthDrivers/Forward/__init__.py
@@ -7,8 +7,8 @@ class SynthDriver(SynthDriver):
 	"""ForwardTacotron For NVDA.
 	Author: Mateo C"""
 	name="Forward"
-	# Translators: Description for the speech synthesizer.
 	description=_("ForwardTacotron")
+	factor = 2 # maximum rate/pitch/energy (in 2.0x).
 
 	@classmethod
 	def check(cls):
@@ -19,51 +19,53 @@ class SynthDriver(SynthDriver):
 	#_availableVoices = OrderedDict({name: synthDriverHandler.VoiceInfo(name, description)})
 
 	def __init__(self):
+		super().__init__()
 		self.rate=1 # normal rate: 1.0
 		self.pitch=1
 		self.energy=1
-		self.factor = 2 # maximum rate/pitch/energy (in 2.0x).
 		self.vocoder="hifigan" # hifigan, griffinlim. A combo box could be made to select it in a future release.
 		self.freq=22050 # sample rate for HiFi-GAN inference. 22050 and 32000 are currently supported.
 
 	PROSODY_ATTRS = {RateCommand: "rate", PitchCommand: "pitch"}
 
 	# These are functions to convert from percent to x, x to percent, and vice versa. ForwardTacotron uses the parameters based on x.
-	def percent_to_x(self, percent, factor):
-		return (percent / 100) * factor
+	def percent_to_x(self, percent):
+		return (percent / 100) * self.factor
 
-	def x_to__percent(self, x, factor):
-		return (x/factor) * 100
+	def x_to_percent(self, x):
+		return int(x/self.factor * 100)
 
 	def _get_rate_percent(self):
-		return self.x_to__percent(self.rate, 2)
+		return int(self.x_to_percent(self.rate, 2))
 
 	def _get_rate(self):
-		return self._get_rate_percent
+		return self._get_rate_percent()
 
 	def _get_pitch_percent(self):
-		return self.x_to__percent(self.pitch, 2)
+		return self.x_to_percent(self.pitch, 2)
 
 	def _get_pitch(self):
-		return self._get_pitch_percent
+		return self._get_pitch_percent()
 
-	def _set_rate(self,rate):
-		self.Rate = self.prosody_percent_to_x(rate, 2)
+	def _set_rate(self, value):
+		#self.rate = self.percent_to_x(value)
+		self.rate = value
 
 	def _set_pitch(self,pitch):
-		self._pitch=self.prosody_percent_to_x(pitch, 2)
+		self.pitch=self.percent_to_x(pitch)
 
 	def speak(self, speechSequence):
 		self.lastIndex = None
 		for item in speechSequence:
 			if isinstance(item, IndexCommand):
 				self.lastIndex = item.index
-				client.send_text(speechSequence, vocoder = self.vocoder, freq = self.freq, rate = self.rate, pitch = self.pitch, energy = self.energy)
 			elif type(item) in self.PROSODY_ATTRS:
-				if type(item) == RateCommand:
-					print(item)
-				if type(item) == PitchCommand:
-					print(item)
+				if isinstance(item, RateCommand):
+					self.rate = self.percent_to_x(item.value)
+				elif isinstance(item, PitchCommand):
+					self.pitch = self.percent_to_x(item.value)
+		client.send_text(speechSequence, vocoder=self.vocoder, freq=self.freq, rate=self.rate, pitch=self.pitch, energy=self.energy)
+
 	def cancel(self):
 		self.lastIndex = None
 
